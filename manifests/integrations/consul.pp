7ff0210907314cec7cbfeff18539349a3833d58b
William Tsoi <william@williamtsoi.net>
2016-04-15 15:17:52 +1000

    Update consul.pp

--- manifests/integrations/consul.pp
+++ manifests/integrations/consul.pp
@@ -1,4 +1,4 @@
-# Class: datadog_agent::integrations::consul
+# Class: datadog_agent_windows::integrations::consul
 #
 # This class will install the necessary configuration for the consul integration
 #
@@ -8,21 +8,21 @@
 #
 # Sample Usage:
 #
-#   class { 'datadog_agent::integrations::consul' :
+#   class { 'datadog_agent_windows::integrations::consul' :
 #     url  => "http://localhost:8500"
 #   }
 #
-class datadog_agent::integrations::consul(
+class datadog_agent_windows::integrations::consul(
   $url = 'http://localhost:8500'
-) inherits datadog_agent::params {
+) inherits datadog_agent_windows::params {
 
-  file { "${datadog_agent::params::conf_dir}/consul.yaml":
+  file { "${datadog_agent_windows::params::conf_dir}/consul.yaml":
     ensure  => file,
-    owner   => $datadog_agent::params::dd_user,
-    group   => $datadog_agent::params::dd_group,
+    owner   => $datadog_agent_windows::params::dd_user,
+    group   => $datadog_agent_windows::params::dd_group,
     mode    => '0644',
-    content => template('datadog_agent/agent-conf.d/consul.yaml.erb'),
-    require => Package[$datadog_agent::params::package_name],
-    notify  => Service[$datadog_agent::params::service_name]
+    content => template('datadog_agent_windows/agent-conf.d/consul.yaml.erb'),
+    require => Package[$datadog_agent_windows::params::package_name],
+    notify  => Service[$datadog_agent_windows::params::service_name]
   }
 }
